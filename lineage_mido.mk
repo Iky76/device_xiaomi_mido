dd7bbd6578c5a017c76d074928d5ab6cc151e3e0
Iky76 <yuyenem@gmail.com>
2026-04-29 19:07:16 +0000

    mido:Bringup lineageOS
    Signed-off-by: Iky76 <yuyenem@gmail.com>

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -23,20 +23,15 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # PixysOS flags
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-
-#Gapps
-TARGET_GAPPS_ARCH := arm64
 
 # Inherit some common PixysOS stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := pixys_mido
+PRODUCT_NAME := lineage_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
